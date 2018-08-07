22f6e4b941de1fe4464773a6fb4310e32c30a49e
Tian Zhang <tizhang@CNtizhang.local>
2018-08-07 13:37:36 +0800

    move user service
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/main/java/com/thoughtworks/training/zhangtian/todoservice/feign/UserFeign.java
+++ src/main/java/com/thoughtworks/training/zhangtian/todoservice/feign/UserFeign.java
@@ -4,7 +4,7 @@ import com.thoughtworks.training.zhangtian.todoservice.model.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "user", url = "http://localhost:8081/api")
+@FeignClient(name = "user", url = "http://localhost:8081")
 public interface UserFeign {
     @PostMapping("/validate")
     User validateUser(String token);
